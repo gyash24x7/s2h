474bedec9946672edbf23486bad01e3442665a0e
Yash Gupta <gyash24x7@gmail.com>
2022-02-10 14:46:09 +0530

    added trpc routers to backend app

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,16 +1,36 @@
 import express from "express";
+import http from "http";
+import ws from "ws";
+import dotenv from "dotenv";
+import { createTrpcMiddleware } from "@s2h/routers/app";
 import { PrismaClient } from "@s2h/prisma";
+import { handleGoogleAuth } from "@s2h/routers/users";
+
+dotenv.config();
 
 const app = express();
-const prisma = new PrismaClient( { log: [ "query" ] } );
+const server = http.createServer( app );
+const wss = new ws.Server( { server } );
+const prisma = new PrismaClient();
 
-app.get( "/api", async ( req, res ) => {
-	await prisma.user.findMany();
-	res.send( { message: "Welcome to backend!" } );
+app.use( "/trpc", createTrpcMiddleware( prisma ) );
+
+app.get( "/", ( _req, res ) => {
+	res.send( { hello: false } );
 } );
 
-const port = process.env.port || 3333;
-const server = app.listen( port, () => {
-	console.log( `Listening at http://localhost:${ port }/api` );
+app.get( "/oauth/google/redirect", handleGoogleAuth( prisma ) );
+
+wss.on( "connection", ( ws ) => {
+
+	ws.on( "message", ( message: string ) => {
+		console.log( "received: %s", message );
+		ws.send( `Hello, you sent -> ${ message }` );
+	} );
+
+	ws.send( "Hi there, I am a WebSocket server" );
 } );
-server.on( "error", console.error );
+
+server.listen( process.env.PORT || 8000, () => {
+	console.log( `Server started on port ${ process.env.PORT || 8000 }` );
+} );
--- libs/utils/trpc/src/index.ts
+++ libs/utils/trpc/src/index.ts
@@ -1,6 +1,5 @@
 import type { Request, Response } from "express";
 import type { PrismaClient } from "@s2h/prisma";
-import type { ProcedureResolver } from "@trpc/server/src/internals/procedure";
 
 export type TrpcContext = {
 	req: Request;
@@ -9,6 +8,11 @@ export type TrpcContext = {
 	prisma: PrismaClient
 }
 
-export type TrpcResolver<I = any, R = any> = ProcedureResolver<TrpcContext, I, R>;
+export type TrpcResolverOptions<I = any> = {
+	input: I;
+	ctx: TrpcContext;
+}
+
+export type TrpcResolver<I = any, R = any> = ( options: TrpcResolverOptions<I> ) => R | Promise<R>
 
 export type ExpressResolver = ( prisma: PrismaClient ) => ( req: Request, res: Response ) => void | Promise<void>
