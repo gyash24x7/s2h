5660575271b0e1b3666e4678424c5efc088ba321
Yash Gupta <gyash24x7@gmail.com>
2022-02-20 23:43:39 +0530

    added strict tsconfig for backend app

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import http from "http";
 import ws from "ws";
 
+const port = process.env[ "PORT" ] || 8000;
+
 const app = express();
 const server = http.createServer( app );
 const wss = new ws.Server( { server } );
@@ -26,6 +28,6 @@ wss.on( "connection", ( ws ) => {
 	ws.send( "Hi there, I am a WebSocket server" );
 } );
 
-server.listen( process.env.PORT || 8000, () => {
-	console.log( `Server started on port ${ process.env.PORT || 8000 }` );
+server.listen( port, () => {
+	console.log( `Server started on port ${ port }` );
 } );
