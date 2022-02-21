9912e1ae7899898cb3e250bedce1db5f5e613b33
Yash Gupta <gyash24x7@gmail.com>
2022-02-21 20:37:02 +0530

    integrated routers with backend app

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import http from "http";
 import ws from "ws";
 import { PrismaClient } from "@s2h/prisma";
+import { createTrpcMiddleware } from "@s2h/routers";
+import { handleGoogleAuth } from "@s2h/routers/users";
 
 const port = process.env[ "PORT" ] || 8000;
 
@@ -10,14 +12,14 @@ const server = http.createServer( app );
 const wss = new ws.Server( { server } );
 const prisma = new PrismaClient( { log: [ "query" ] } );
 
-// app.use( "/trpc", createTrpcMiddleware( prisma ) );
+app.use( "/trpc", createTrpcMiddleware( prisma ) );
 
 app.get( "/", async ( _req, res ) => {
 	await prisma.account.findMany();
 	res.send( { hello: false } );
 } );
 
-// app.get( "/oauth/google/redirect", handleGoogleAuth( prisma ) );
+app.get( "/oauth/google/redirect", handleGoogleAuth( prisma ) );
 
 wss.on( "connection", ( ws ) => {
 
