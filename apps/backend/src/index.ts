ba9a3ea40c67730babb78c3586731bd025a68085
Yash Gupta <gyash24x7@gmail.com>
2022-02-22 00:18:42 +0530

    added morgan and disabled ws and added proxy

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,16 +1,17 @@
 import express from "express";
-import http from "http";
-import ws from "ws";
 import { PrismaClient } from "@s2h/prisma";
 import { createTrpcMiddleware } from "@s2h/routers";
 import { handleGoogleAuth } from "@s2h/routers/users";
+import morgan from "morgan";
 
 const port = process.env[ "PORT" ] || 8000;
 
 const app = express();
-const server = http.createServer( app );
-const wss = new ws.Server( { server } );
+// const server = http.createServer( app );
+// const wss = new ws.Server( { server } );
 const prisma = new PrismaClient( { log: [ "query" ] } );
+
+app.use( morgan( "tiny" ) );
 
 app.use( "/trpc", createTrpcMiddleware( prisma ) );
 
@@ -21,16 +22,16 @@ app.get( "/", async ( _req, res ) => {
 
 app.get( "/oauth/google/redirect", handleGoogleAuth( prisma ) );
 
-wss.on( "connection", ( ws ) => {
+// wss.on( "connection", ( ws ) => {
+//
+// 	ws.on( "message", ( message: string ) => {
+// 		console.log( "received: %s", message );
+// 		ws.send( `Hello, you sent -> ${ message }` );
+// 	} );
+//
+// 	ws.send( "Hi there, I am a WebSocket server" );
+// } );
 
-	ws.on( "message", ( message: string ) => {
-		console.log( "received: %s", message );
-		ws.send( `Hello, you sent -> ${ message }` );
-	} );
-
-	ws.send( "Hi there, I am a WebSocket server" );
-} );
-
-server.listen( port, () => {
+app.listen( port, () => {
 	console.log( `Server started on port ${ port }` );
 } );
--- libs/routers/index.ts
+++ libs/routers/index.ts
@@ -2,8 +2,10 @@ import type { PrismaClient } from "@s2h/prisma";
 import { CreateExpressContextOptions, createExpressMiddleware } from "@trpc/server/adapters/express";
 import * as trpc from "@trpc/server";
 import type { TrpcContext } from "@s2h/utils";
+import { literatureRouter } from "./literature";
+import { usersRouter } from "./users";
 
-export const appRouter = trpc.router<TrpcContext>();
+export const appRouter = trpc.router<TrpcContext>().merge( literatureRouter ).merge( usersRouter );
 
 export type AppRouter = typeof appRouter;
 
